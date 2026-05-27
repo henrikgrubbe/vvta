7c4c2ccd9f9ed618d93c7d90c0cf51683cecc932
Henrik Hersbt Grubbe <henrikhgrubbe@gmail.com>
2026-05-27 07:38:35 +0200

    chore: integrate import-sorting and Prettier into ESLint
    - Add simple-import-sort and prettier integration to ESLint config (enforce imports and surface Prettier errors via ESLint)

--- eslint.config.js
+++ eslint.config.js
@@ -12,7 +12,11 @@ module.exports = defineConfig([
       tseslint.configs.recommended,
       tseslint.configs.stylistic,
       angular.configs.tsRecommended,
+      // Sort imports consistently and run Prettier as part of ESLint so teams can use a single lint entrypoint
+      'plugin:simple-import-sort/recommended',
+      'plugin:prettier/recommended',
     ],
+    plugins: ['simple-import-sort', 'prettier'],
     processor: angular.processInlineTemplates,
     rules: {
       '@angular-eslint/directive-selector': [
@@ -31,11 +35,19 @@ module.exports = defineConfig([
           style: 'kebab-case',
         },
       ],
+      // Enforce import sorting
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      // Surface Prettier formatting issues as ESLint errors so CI/lint is a single entrypoint
+      'prettier/prettier': 'error',
     },
   },
   {
     files: ['**/*.html'],
     extends: [angular.configs.templateRecommended, angular.configs.templateAccessibility],
-    rules: {},
+    rules: {
+      // Surface Prettier format issues in templates as ESLint errors as well
+      'prettier/prettier': 'error',
+    },
   },
 ]);
